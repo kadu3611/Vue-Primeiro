8a675014bfd0bf0705d90b1c4dfb74f1413df904
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2022-08-31 08:59:21 -0300

    Pasta Vews

---
+++ vue-primeiro/src/Views/About.vue
@@ -0,0 +1,20 @@
+<template>
+<div class='center'>
+A forma mais simples de testar Vue.js é usando o exemplo de Olá Mundo no CodeSandbox. Sinta-se à vontade para abrí-lo em outra aba e acompanhar conosco durante alguns exemplos básicos.
+<div>
+<router-link to="/config"> Próximo </router-link>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      msg: 'About'
+    }
+  }
+}
+</script>
+<style src="./style.css" scoped />
---
+++ vue-primeiro/src/Views/Config.vue
@@ -0,0 +1,20 @@
+<template>
+<div class='center'>
+No núcleo do Vue.js está um sistema que nos permite declarativamente renderizar dados no DOM (Document Object Model) usando uma sintaxe de template simples
+<div>
+<router-link to="/"> Voltar </router-link>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Config',
+  data () {
+    return {
+      msg: 'Config'
+    }
+  }
+}
+</script>
+<style src="./style.css" scoped />
