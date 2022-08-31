a9aeda91878a613149ca672735da3e4c9582444e
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2022-08-31 09:00:15 -0300

    index rota ok

--- vue-primeiro/src/router/index.js
+++ vue-primeiro/src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import About from '../Views/About'
+import Config from '../Views/Config'
+import Home from '../Views/Home'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/config',
+      name: 'Config',
+      component: Config
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
